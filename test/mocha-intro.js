c92540e920f45221ff3554219a796283ef22772f
Guillermo Abrego <lguille1991@gmail.com>
2021-08-23 20:47:34 -0600

    Added first mocha test

---
+++ test/mocha-intro.js
@@ -0,0 +1,37 @@
+let assert = require('assert');
+
+describe ('Mathematical Operations - Test Suite', function() {
+
+    it ('Add two numbers', function() {
+        let a = 10;
+        let b = 10;
+        let c = a + b;
+
+        assert.equal(c, 20);
+    });
+
+    it ('Substrat two numbers', function() {
+        let a = 10;
+        let b = 10;
+        let c = a - b;
+
+        assert.equal(c, 0);
+    });
+
+    it ('Multiply two numbers', function() {
+        let a = 10;
+        let b = 10;
+        let c = a * b;
+
+        assert.equal(c, 100);
+    });
+
+    it ('Divide two numbers', function() {
+        let a = 10;
+        let b = 10;
+        let c = a / b;
+
+        assert.equal(c, 1);
+    });
+
+});
